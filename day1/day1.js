0ddbd4ed26ff3f90380669b653e9e2850c960dd6
Aslak Frafjord Skailand <AslakFrafjord.Skailand@Altibox.no>
2021-12-04 11:45:48 +0100

    Added part two for day 1.

--- day1/day1.js
+++ day1/day1.js
@@ -3,6 +3,8 @@ var fs = require("fs");
 const depths = fs.readFileSync("puzzleInput.txt").toString().split("\n");
 let depthIncreaseCounter = 1; // starts at 1 since the first measurement is bigger than undefined :)
 
+// Part one
+
 depths.forEach((depth, index, depths) => {
   if (index === 0) {
     return;
@@ -15,4 +17,29 @@ depths.forEach((depth, index, depths) => {
 console.log(
   "Number of measurements larger than the previous measurement:",
   depthIncreaseCounter
+);
+
+// Part two
+
+const add = (sum, a) => {
+  return Number(sum) + Number(a);
+};
+
+let prevDepth = 0;
+let counter = 0;
+
+depths.forEach((_, index) => {
+  if (index === 0) {
+    return;
+  }
+  const calculatedDepth = depths.slice(index, index + 3).reduce(add, 0);
+  if (calculatedDepth > prevDepth) {
+    counter++;
+  }
+  prevDepth = calculatedDepth;
+});
+
+console.log(
+  "Number of measurements larger than the previous measurement:",
+  counter
 );
